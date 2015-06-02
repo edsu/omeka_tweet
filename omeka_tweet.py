dd5999453ba94be38040301dde1e5232b6d94770
Ed Summers <ehs@pobox.com>
2015-06-02 15:18:38 +0000

    refactored to handle shortening status text when it is greater than 140 chars

--- omeka_tweet.py
+++ omeka_tweet.py
@@ -21,6 +21,7 @@ ACCESS_TOKEN = os.environ.get("ACCESS_TOKEN")
 ACCESS_SECRET = os.environ.get("ACCESS_SECRET")
 SEEN_DB = os.environ.get("SEEN_DB", "omeka-tweet.json")
 FEED_URL = os.environ.get("FEED_URL")
+SLEEP_SECONDS = int(os.environ.get("SLEEP_SECONDS", "20"))
 
 if os.path.isfile(SEEN_DB):
     seen = json.load(open(SEEN_DB))
@@ -31,11 +32,20 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
 twitter = tweepy.API(auth)
 
-feed = ElementTree.parse(urllib.urlopen(FEED_URL)).getroot()
 
-for item in feed.findall('.//item'):
-    title = item.find('title').text
+def main():
+    feed = ElementTree.parse(urllib.urlopen(FEED_URL)).getroot()
+    for item in feed.findall('.//item'):
+        if tweet(item):
+            time.sleep(SLEEP_SECONDS)
+
+
+def tweet(item):
     link = item.find('link').text
+    if link in seen:
+        return False
+
+    title = item.find('title').text
     desc = item.find('description').text
     encl = item.find('enclosure')
     if encl is not None: 
@@ -47,31 +57,58 @@ for item in feed.findall('.//item'):
     desc = soup.select('div[id="dublin-core-description"] div')[0].text
     creator = soup.select('div[id="dublin-core-creator"] div')[0].text
 
-    if link not in seen:
-        if title != "[Untitled]":
-            status = "%s by %s %s" % (title, creator, link)
-        else:
-            status = "%s by %s %s" % (desc, creator, link)
+    status = get_status_text(title, desc, creator, link, img)
 
-        if img:
-            fh, path = tempfile.mkstemp()
-            urllib.urlretrieve(img, path)
+    if img:
+        png = get_image(img)
+        twitter.update_with_media(png, status)
+        os.remove(png)
+    else:
+        twitter.update_status(status=status)
 
-            # twitter doesn't like all jpegs so convert to png
+    seen[link] = True
+    json.dump(seen, open(SEEN_DB, "w"), indent=2)
 
-            i = Image.open(path)
-            png = path + '.png'
-            i.save(png, 'png')
+    return True
 
-            twitter.update_with_media(png, status)
 
-            os.remove(path)
-            os.remove(png)
+def get_status_text(title, desc, creator, link, img):
+    # urls for web pages and embedded media take up 23 chars so adjust
+    # the status message to fit within the allotted 140 characters
+    limit = 140 - 23
+    if img:
+        limit -= 23
 
-        else:
-            twitter.update_status(status=status)
+    # not all Omeka items have titles
+    if title != "[Untitled]":
+        msg = title
+    else:
+        msg = desc
 
-        seen[link] = True
-        json.dump(seen, open(SEEN_DB, "w"), indent=2)
+    # see if we can add the creator to the message
+    if len(msg) > limit:
+        msg = msg[0:limit]
+    elif len(msg + ' by ' + creator) < limit:
+        msg = msg + ' by ' + creator
 
-        time.sleep(20)
+    return "%s %s" % (msg, link)
+
+
+def get_image(url):
+    fh, path = tempfile.mkstemp()
+    urllib.urlretrieve(url, path)
+
+    # twitter doesn't like all jpegs so convert to png
+    i = Image.open(path)
+    png = path + '.png'
+    i.save(png, 'png')
+
+    # clean up original
+    os.close(fh)
+    os.remove(path)
+
+    return png
+
+
+if __name__ == "__main__":
+    main()
